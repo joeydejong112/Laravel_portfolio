7b5b35b609a07119327607c6e7e32e742fd46072
oek <oke>
2020-03-28 01:52:03 +0100

    basic setup

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+
+
+  // Hero typed
+  var typed_strings = $(".typed").data('typed-items');
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+  // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+  
+
+  // Initi AOS
+  AOS.init({
+    duration: 1000,
+    disable: function() {
+      var maxWidth = 800;
+      return window.innerWidth < maxWidth;
+    }
+  });
+
